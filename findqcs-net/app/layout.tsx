ce083c83c963736fbfeb603d507e1f85a0194131
hehaize88-code <hehaize88@gmail.com>
2026-08-26 15:58:07 +0800

    feat(analytics): add GA4 for findqcs-net

--- findqcs-net/app/layout.tsx
+++ findqcs-net/app/layout.tsx
@@ -12,5 +12,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="en"><body><script async src="https://www.googletagmanager.com/gtag/js?id=G-80350EE2X8" /><script dangerouslySetInnerHTML={{ __html: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-80350EE2X8');" }} />{children}</body></html>;
 }
