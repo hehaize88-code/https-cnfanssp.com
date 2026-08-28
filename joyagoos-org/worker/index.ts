9305fb972d02ec766de192ad3dec248574387bee
hehaize88-code <hehaize88@gmail.com>
2026-08-28 14:32:38 +0800

    Serve Joyagoos robots and sitemap directly

--- joyagoos-org/worker/index.ts
+++ joyagoos-org/worker/index.ts
@@ -1,6 +1,29 @@
 /** Cloudflare Worker entry point for joyagoos.org. */
 import handler from "vinext/server/app-router-entry";
 
+const siteOrigin = "https://joyagoos.org";
+const languages = ["en", "de", "es", "fr", "it"];
+const pages = [
+  "",
+  "spreadsheet/",
+  "finds/",
+  "guide/",
+  "qc/",
+  "shipping/",
+  "faq/",
+  "articles/",
+  "articles/joyagoo-buying-fees-guide/",
+  "articles/joyagoo-qc-return-window-guide/",
+  "articles/joyagoo-warehouse-rehearsal-shipping-guide/",
+  "articles/volumetric-weight-guide/",
+  "articles/joyagoo-reviews-buyer-signals/",
+];
+
+const robotsText = `User-agent: *\nAllow: /\nSitemap: ${siteOrigin}/sitemap.xml\n`;
+const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${languages
+  .flatMap((language) => pages.map((page) => `  <url><loc>${siteOrigin}/${language}/${page}</loc><lastmod>2026-08-28</lastmod></url>`))
+  .join("\n")}\n</urlset>\n`;
+
 interface Env {
   ASSETS: Fetcher;
 }
@@ -12,6 +35,13 @@ interface ExecutionContext {
 
 const worker = {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const { pathname } = new URL(request.url);
+    if (pathname === "/robots.txt") {
+      return new Response(robotsText, { headers: { "content-type": "text/plain; charset=utf-8" } });
+    }
+    if (pathname === "/sitemap.xml") {
+      return new Response(sitemapXml, { headers: { "content-type": "application/xml; charset=utf-8" } });
+    }
     return handler.fetch(request, env, ctx);
   },
 };
