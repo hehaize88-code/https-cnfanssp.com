b20f7495aec9a2c04fff54ab4528ac78006cd9ac
hehaize88-code <hehaize88@gmail.com>
2026-09-21 17:18:11 +0800

    Add localized Joyagoo article hub route

---
+++ joyagoos-store/app/[locale]/articles-2026/page.tsx
@@ -0,0 +1,23 @@
+import { notFound } from "next/navigation";
+import { ArticlesContent } from "../../articles-content";
+import { resolveLanguage } from "../../i18n";
+import { buildPageMetadata } from "../../seo";
+import { locales, localizedSections, type Locale } from "../../site-data";
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  if (!(locale in locales)) return {};
+  const content = localizedSections[locale as Locale].articles;
+  return buildPageMetadata({
+    title: `${content.title} | ${locales[locale as Locale].label}`,
+    description: content.intro,
+    path: "/articles",
+    language: locale as Locale,
+  });
+}
+
+export default async function Page({ params, searchParams }: { params: Promise<{ locale: string }>; searchParams: Promise<{ lang?: string | string[] }> }) {
+  const [{ locale }, query] = await Promise.all([params, searchParams]);
+  if (!(locale in locales)) notFound();
+  return <ArticlesContent initialLocale={resolveLanguage(query.lang, locale as Locale)} prefix={`/${locale}`} />;
+}
